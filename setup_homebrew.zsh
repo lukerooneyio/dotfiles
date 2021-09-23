b7f536168d33de074da87d5f0ba13d111b072038
Luke Rooney <53066184+lukerooneyio@users.noreply.github.com>
2021-09-23 12:55:42 -0700

    Use --no-quarantine for all apps

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -7,4 +7,5 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 brew install httpie
 brew install bat
 
-brew install visual-studio-code
+brew install --no-quarantine  visual-studio-code
+brew install --no-quarantine
